d1c9173f3b563a57bac9f3232601ae622976359c
TongWee <308992640@qq.com>
2015-12-09 21:55:42 +0800

    code format

--- src/garbagecollection/MemoryLeak.java
+++ src/garbagecollection/MemoryLeak.java
@@ -4,22 +4,15 @@ import java.util.HashMap;
 import java.util.Vector;
 
 public class MemoryLeak {
-	//静态集合类如果
+	// 静态集合类如果
 	public static Vector vector = new Vector();
 	public static HashMap hashMap = new HashMap();
-	//数据库连接、网络连接、IO连接
-	//监听器
+	// 数据库连接、网络连接、IO连接
+	// 监听器
 	/*
-	 * Button btn = new Button();
-	 * btn.setOnClickListener(new OnClickListener(
-	 * @Override
-	 * void onCLick(){
-	 * 		System.out.println("Button down.");
-	 * }
-	 * ))
-	 *
+	 * Button btn = new Button(); btn.setOnClickListener(new OnClickListener(
+	 * @Override void onCLick(){ System.out.println("Button down."); } ))
 	 */
-	 //变量不合理的作用域
-	 
+	// 变量不合理的作用域
 
 }
